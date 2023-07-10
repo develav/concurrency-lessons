b6e1c79a1f964a2b321da67e80c90f79e378114f
fmiat <eee@>
2023-07-10 22:40:39 +0300

    исправления: решение в одном функциональном блоке, executor вынесен из метода

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -2,14 +2,12 @@ package course.concurrency.m2_async.cf.min_price;
 
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class PriceAggregator {
 
@@ -21,31 +19,24 @@ public class PriceAggregator {
 
     private Collection<Long> shopIds = Set.of(10l, 45l, 66l, 345l, 234l, 333l, 67l, 123l, 768l);
 
+    private ExecutorService executorService = Executors.newCachedThreadPool();
+
     public void setShops(Collection<Long> shopIds) {
         this.shopIds = shopIds;
     }
 
     public double getMinPrice(long itemId) {
 
-        ExecutorService executorService = Executors.newFixedThreadPool(shopIds.size());
-
-        List<CompletableFuture<Double>> list = shopIds.stream()
+        return shopIds.stream()
                 .map(shopId -> CompletableFuture
                         .supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), executorService)
-                        .orTimeout(2900, TimeUnit.MILLISECONDS)
+                        .orTimeout(2500, TimeUnit.MILLISECONDS)
                         .exceptionally(t -> Double.NaN)
                 )
-                .collect(Collectors.toList());
-
-        Stream<Double> doubleStream = list.stream().map(x -> {
-            Double aDouble = Double.MAX_VALUE;
-            try {
-                aDouble = x.get();
-            } catch (Exception e) {
-            }
-            return aDouble;
-        });
-
-        return doubleStream.min(Comparator.naturalOrder()).get();
+                .collect(Collectors.toList())
+                .stream()
+                .map(CompletableFuture::join)
+                .min(Comparator.naturalOrder())
+                .get();
     }
 }
