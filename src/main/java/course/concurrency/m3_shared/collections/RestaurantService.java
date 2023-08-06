a9f9e615dfccfbbe6e05130e521f74a325e20cad
fmiat <eee@>
2023-08-06 14:03:46 +0300

    решение задачи 3-11

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.stream.Collectors;
 
 public class RestaurantService {
 
@@ -13,7 +15,7 @@ public class RestaurantService {
         put("C", new Restaurant("C"));
     }};
 
-    private Object stat;
+    private ConcurrentHashMap<String, Integer> stat = new ConcurrentHashMap<>();
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -21,11 +23,16 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        // your code
+        stat.merge(restaurantName, 1, Integer::sum);
     }
 
     public Set<String> printStat() {
-        // your code
-        return new HashSet<>();
+        return stat
+                .entrySet()
+                .stream()
+                .map(element -> element.getKey() + " - " + element.getValue())
+                .collect(Collectors.toSet());
     }
+
+
 }
